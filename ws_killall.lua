fe4d206b318b93caae635e6d890645c0afa723c4
KryptonMiner <79050258+KryptonMiner@users.noreply.github.com>
2021-10-12 10:38:29 +0200

    Create ws_killall.lua

---
+++ ws_killall.lua
@@ -0,0 +1,22 @@
+local sndujfjnsdffsdnj = game:GetService("Players").LocalPlayer.Name
+for i, zujhgjhgjghjghj in pairs(game:GetService'Players':GetChildren()) do
+if zujhgjhgjghjghj and zujhgjhgjghjghj.Character then
+if zujhgjhgjghjghj.Name == sndujfjnsdffsdnj then 
+    else
+local dfgfgdfdgfgdfdgfgd = zujhgjhgjghjghj.Character
+local args = {
+    [1] = dfgfgdfdgfgdfdgfgd.Humanoid,
+    [2] = 9e9
+}
+
+game:GetService("ReplicatedStorage").Attack:FireServer(unpack(args))
+wait(.1)
+local args = {
+    [1] = dfgfgdfdgfgdfdgfgd.Humanoid,
+    [2] = 9e9
+}
+
+game:GetService("ReplicatedStorage").Attack:FireServer(unpack(args))
+end
+end
+end
